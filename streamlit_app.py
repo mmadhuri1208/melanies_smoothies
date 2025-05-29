0c54f50009e23c7ee970b7a8a2d6a3948f83db1c
mmadhuri1208 <36484930+mmadhuri1208@users.noreply.github.com>
2025-05-29 00:56:20 -0500

    Update streamlit_app.py
    added 'SEARCH_ON' column

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
 # Multiselect fruit options
 ingredients_list = st.multiselect(
